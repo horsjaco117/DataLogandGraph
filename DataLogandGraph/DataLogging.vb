e223c66ad0c12caeb027815ffbbafdb579c0151c
horsjaco117 <jacob_horsley@live.com>
2025-10-29 11:01:08 -0600

    Random number stuff is working.

--- DataLogandGraph/DataLogging.vb
+++ DataLogandGraph/DataLogging.vb
@@ -1,6 +1,27 @@
-﻿Public Class DataLoggingGraph
+﻿Option Strict On
+Option Explicit On
+Imports System.Runtime.CompilerServices
+Public Class DataLoggingGraph
 
     'Program logic---------------------------------------------------------------------------
+    Function GetRandomNumberAround(thisNumber%, Optional within% = 10) As Integer
+        Dim result%
+        result = thisNumber - within
+        result += (GetRandomNumber(within) \ 2) + (GetRandomNumber(within))
+
+
+        'Another way to do this
+        'result = (GetRandomNumber(2 * within) + (thisNumber - within))
+
+        Return result
+    End Function
+
+
+    Function GetRandomNumber(max%) As Integer
+        Randomize()
+
+        Return CInt(System.Math.Floor((Rnd() * max + 1)))
+    End Function
 
     Function GetData() As Integer
         Return 5
@@ -9,8 +30,8 @@
     Sub GraphData()
         Dim g As Graphics = GraphPictureBox.CreateGraphics
         Dim pen As New Pen(Color.Lime)
-        Dim scaleX As Single = GraphPictureBox.Width \ 100
-        Dim scaleY As Single = (GraphPictureBox.Height \ 100) * -1
+        Dim scaleX As Single = CSng(GraphPictureBox.Width \ 100)
+        Dim scaleY As Single = CSng((GraphPictureBox.Height \ 100) * -1)
 
         g.TranslateTransform(0, GraphPictureBox.Height)
         g.ScaleTransform(scaleX, scaleY)
@@ -31,6 +52,9 @@
     End Sub
 
     Private Sub GraphButton_Click(sender As Object, e As EventArgs) Handles GraphButton.Click
-        GraphData()
+        'GraphData()
+        For i = 1 To 100
+            Console.WriteLine(GetRandomNumberAround(50, 10))
+        Next
     End Sub
 End Class
